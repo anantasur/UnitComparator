081f8673ba340efa5656fa0c8641ccacc85524a1
Ananthu R V <ananthurv666@hotmail.com>
2015-03-16 16:54:07 +0530

    added more tests to check comparison of a comparator type

--- Test/com/unit/ComparatorTest.java
+++ Test/com/unit/ComparatorTest.java
@@ -17,8 +17,20 @@ public class ComparatorTest {
         assertEquals(39370.1, c.convert(c), 0.0);
     }
     @Test
-    public void Test1LitersIsEqualToPoint264172Gallon (){
-        Volume v = new Volume(1.0, UNITTYPE.LITERS);
+    public void Test1LitersIsEqualToPoint264172Gallon () {
+        Comparator v = new Comparator(1.0, UNITTYPE.LITERS);
         assertEquals(0.264172, v.convert(v), 0.0);
+    }
+
+    @Test
+    public void Test1gallonIsEqualTo3_78541liters (){
+        Comparator v = new Comparator(1.0, UNITTYPE.GALLON);
+        assertTrue(v.compare(new Comparator(3.7854, UNITTYPE.LITERS)));
+    }
+
+    @Test
+    public void Test2inchIsEqualTo5_08centimeters (){
+        Comparator c = new Comparator(2.0,UNITTYPE.INCH);
+        assertTrue(c.compare(new Comparator(5.08, UNITTYPE.CM)));
     }
 }
